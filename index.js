b322c6335f8ec359253e1b608327ddaf9505d0a6
hosn <xuhosn@gmail.com>
2015-09-22 13:16:33 +0800

    Demo

---
+++ index.js
@@ -0,0 +1,28 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/hello', function(req, res) {
+    res.send('Hello there!');
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
