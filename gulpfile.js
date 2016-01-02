185d20a666566d011d026531e9998538dab0ec42
hosn <xuhosn@gmail.com>
2016-01-02 22:16:09 +0800

    change to mdl and vue

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,41 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var jade = require('gulp-jade');
-var reload = browserSync.roload;
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
-gulp.task('templates', function () {
-    return gulp.src('./views/*.jade')
-        .pipe(jade())
-        .pipe(gulp.dest('./dist'))
-        .pipe(browserSync.stream());
+gulp.task('sass', function () {
+  return gulp.src('./dev/sass/main.sass')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('./static/css'))
+    .on('end', browserSync.reload);
+})
+
+gulp.task('jade', function () {
+  return gulp.src('./views/*.jade')
+    .on('end', browserSync.reload);
 });
 
-gulp.task('jade-watch', ['templates'], reload);
+gulp.task('scripts', function () {
+  return gulp.src('./dev/js/main.js')
+    .pipe(browserify())
+    .pipe(rename('bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./static/js'))
+    .on('end', browserSync.reload);
+});
 
-gulp.task('default', ['templates'], function () {
-    browserSync({
-        server: './dist'
-    });
-    gulp.watch('./views/*.jade', ['jade-watch']);
+gulp.task('default', ['jade', 'scripts', 'sass'], function () {
+
+  browserSync.init({
+    proxy: 'localhost:5000'
+  });
+  gulp.watch('./views/*.jade', ['jade']);
+  gulp.watch('./dev/js/*.js', ['scripts']);
+  gulp.watch('./dev/sass/*.sass', ['sass']);
 });
--- routes/home.js
+++ routes/home.js
@@ -2,14 +2,13 @@
 var User = require('../lib/user');
 var Quiz = require('../lib/quiz');
 var home = function (req, res) {
-    if (req.session.user) {
-        var user = req.session.user;
-        res.locals.username = user;
-        
-        res.render('home');
-    } else {
-        res.redirect('login');
-    }
+  if (req.session.user) {
+    var user = req.session.user;
+    res.locals.username = user.name;
+    res.render('home');
+  } else {
+    res.redirect('login');
+  }
 }
 
 module.exports = home;
